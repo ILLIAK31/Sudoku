3cb9503dca85b9f606421ad56a724d7339eaa717
Illia <karma@MYPC>
2024-10-12 17:01:45 +0200

    12.10.2024

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+public class Main
+{
+    public static class SudokuState
+    {
+        private byte[][] field;
+        private int n , n_puste;
+        public SudokuState(int n)
+        {
+            field = new byte[n*n][n*n];
+            this.n = n;
+            n_puste = n * n * n * n;
+        }
+        @Override
+        public String toString()
+        {
+            StringBuilder s = new StringBuilder();
+            for(int i = 0; i < field.length; i++)
+            {
+                for(int j = 0; j < field[i].length; j++)
+                {
+                    s.append(field[i][j]);
+                }
+                s.append("\n");
+            }
+            return s.toString();
+        }
+        public void fromString(String s)
+        {
+            int idx = 0;
+            n_puste = 0;
+            for (int i = 0; i < field.length; i++) {
+                for (int j = 0; j < field[i].length; j++)
+                {
+                    char c = s.charAt(idx);
+                    if (c == '.' || c == '0')
+                    {
+                        field[i][j] = 0;
+                        n_puste++;
+                    } else
+                    {
+                        field[i][j] = (byte) (c - '0');
+                    }
+                    idx++;
+                }
+            }
+        }
+        /*static boolean isValid(byte[] t)
+        {
+            for(byte ti : t)
+            {
+
+            }
+            return false;
+        }*/
+        public int getEmpty()
+        {
+            return n_puste;
+        }
+    }
+    public static void main(String[] args)
+    {
+        SudokuState obj = new SudokuState(3);
+        System.out.println(obj.toString());
+        obj.fromString("5.2..6.9....49.2..........73.6..59..9.86......7.......2..9.3...68...7..3.5...4...");
+        System.out.println(obj.toString());
+    }
+}

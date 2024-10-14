4ab18b66e1fb31b3b6ccf55389565df78da8017d
Illia <karma@MYPC>
2024-10-14 15:58:52 +0200

    14.10.2024

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import sac.State;
+import sac.StateFunction;
+import sac.graph.BestFirstSearch;
+import sac.graph.GraphState;
+import sac.graph.GraphStateImpl;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 
 public class Main
 {
-    public static class SudokuState
+    public static class SudokuState extends GraphStateImpl
     {
         private byte[][] field;
         private int n , n_puste;
@@ -11,6 +20,18 @@ public class Main
             field = new byte[n*n][n*n];
             this.n = n;
             n_puste = n * n;
+        }
+        public SudokuState(SudokuState s)
+        {
+            this(s.n);
+            this.n_puste = s.n_puste;
+            for(int i=0;i<this.field.length;++i)
+            {
+                for(int j=0;j < this.field[i].length;++j)
+                {
+                    this.field[i][j] = s.field[i][j];
+                }
+            }
         }
         @Override
         public String toString()
@@ -46,42 +67,49 @@ public class Main
                 }
             }
         }
-        public boolean isValid(byte[] t)
+        public boolean isValid()
         {
-            /*
-            if (t.length == 0)
+            HashSet<Byte> seen;
+
+            for (int i = 0; i < field.length; i++)
             {
-                return true;
-            }
-            boolean[] visited = new boolean[t.length];
-            for (int i = 0; i < t.length; i++)
-            {
-                visited[i] = false;
-            }
-            for (byte element : t)
-            {
-                int index = element - 1;
-                if (index < 0 || index >= t.length)
+                seen = new HashSet<>();
+                for (int j = 0; j < field[i].length; j++)
                 {
-                    return false;
-                }
-                if (visited[index])
-                {
-                    return false;
-                } else {
-                    visited[index] = true;
-                }
-            }
-            return true;
-            */
-            HashSet<Byte> seen = new HashSet<>();
-            for (byte value : t)
-            {
-                if (value != 0)
-                {
-                    if (!seen.add(value))
+                    byte rowValue = field[i][j];
+                    if (rowValue != 0 && !seen.add(rowValue))
                     {
                         return false;
+                    }
+                }
+
+                seen.clear();
+                for (int j = 0; j < field.length; j++)
+                {
+                    byte colValue = field[j][i];
+                    if (colValue != 0 && !seen.add(colValue))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            int sqrtN = (int) Math.sqrt(field.length);
+            for (int boxRow = 0; boxRow < field.length; boxRow += sqrtN)
+            {
+                for (int boxCol = 0; boxCol < field.length; boxCol += sqrtN)
+                {
+                    seen = new HashSet<>();
+                    for (int i = 0; i < sqrtN; i++)
+                    {
+                        for (int j = 0; j < sqrtN; j++)
+                        {
+                            byte value = field[boxRow + i][boxCol + j];
+                            if (value != 0 && !seen.add(value))
+                            {
+                                return false;
+                            }
+                        }
                     }
                 }
             }
@@ -91,14 +119,73 @@ public class Main
         {
             return n_puste;
         }
+
+        @Override
+        public List<GraphState> generateChildren()
+        {
+            List<GraphState> lst = new ArrayList<>();
+            for(int i = 0; i < field.length; i++)
+            {
+                for(int j = 0; j < field[i].length; j++)
+                {
+                    if(field[i][j] == 0)
+                    {
+                        for(int k = 1; k <= n*n; k++)
+                        {
+                            field[i][j] = (byte)k;
+                            n_puste--;
+                            if(isValid())
+                            {
+                                lst.add(new SudokuState(this));
+                            }
+                            n_puste++;
+                            field[i][j] = 0;
+                        }
+                        return lst;
+                    }
+                }
+            }
+            return lst;
+        }
+
+        @Override
+        public boolean isSolution()
+        {
+            return n_puste == 0;
+        }
+        @Override
+        public int hashCode()
+        {
+            return toString().hashCode();
+        }
+    }
+    public static class HeurystykaPusteKomorki extends StateFunction
+    {
+        public double calculate(State s)
+        {
+            if(s instanceof SudokuState)
+            {
+                SudokuState ss = (SudokuState)s;
+                return ss.n_puste;
+            }
+            else
+            {
+                return Double.NaN;
+            }
+        }
     }
     public static void main(String[] args)
     {
         SudokuState obj = new SudokuState(3);
         System.out.println(obj.toString());
-        obj.fromString("5.2..6.9....49.2..........73.6..59..9.86......7.......2..9.3...68...7..3.5...4...");
+        obj.fromString(".2.....46.....67....6...5.....928.65..............382.5.4....8.3.9..2....6249.37.");
         System.out.println(obj.toString());
-        System.out.println(obj.isValid(obj.field[0]));
-        System.out.println(obj.getEmpty());
+        System.out.println("Valid : "+obj.isValid()+"\n");
+        obj.setHFunction(new HeurystykaPusteKomorki());
+        BestFirstSearch bfs = new BestFirstSearch();
+        bfs.setInitial(obj);
+        bfs.execute();
+        System.out.println(bfs.getSolutions());
+        // code here
     }
 }
